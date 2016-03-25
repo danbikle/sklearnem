09ed57a998ebc9d2d4d9358f17422051abfa30b2
Dan Bikle <bikle@bikle.com>
2016-03-25 17:39:22 +0000

    hi

--- naive_bayes11.py
+++ naive_bayes11.py
@@ -1,4 +1,13 @@
 # naive_bayes11.py
 
-import prep4fit
+# This script should give demo of sklearn.naive_bayes.GaussianNB
 
+# ref:
+# http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB
+
+import prep4fit
+import pdb
+
+x_train_a = prep4fit.get_x_train_a()
+
+'bye'
