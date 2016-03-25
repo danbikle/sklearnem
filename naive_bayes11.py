0795073b28b667a7c69e940247b8f04ebe0860c0
Dan Bikle <bikle@bikle.com>
2016-03-25 19:30:55 +0000

    hi

--- naive_bayes11.py
+++ naive_bayes11.py
@@ -29,15 +29,13 @@ for xoos_a in x_test_a:
   else:
     predictions_l.append(-1) # down prediction
 
-
-predictions_l
 # I should match the predictions to the test observations.
 test_df         = pd.read_csv('test.csv')
 test_df['pdir'] = predictions_l
 # Get actual_direction of each test observation, which is sign of pctlead
 
 test_df['actual_dir'] = np.sign(test_df['pctlead'])
-test_df.head()
+
 # I should count positive predictions.
 posp_df  = test_df[['pdir','actual_dir']][test_df['pdir'] == 1]
 # I should count true positive predictions.
@@ -52,6 +50,26 @@ tnegp_df = negp_df[negp_df['actual_dir'] == -1]
 # I should calculate negative accuracy.
 neg_acc  = 100.0 * len(tnegp_df)/len(negp_df)
 
-pdb.set_trace()
+# I should calculate combined accuracy
+com_acc  = 100.0 * (len(tposp_df)+len(tnegp_df))/len(test_df)
+
+# I should calculate 'effectiveness'.
+pos_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == 1])
+pos_eff       = np.mean(pos_pctlead_a)
+neg_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == -1])
+neg_eff       = np.mean(neg_pctlead_a)
+longonly_eff  = np.mean(test_df['pctlead'].values)
+# I should report
+print('True  positive count: '+str(len(tposp_df)))
+print('False positive count: '+str(len(posp_df)-len(tposp_df)))
+print('True  negative count: '+str(len(tnegp_df)))
+print('False negative count: '+str(len(negp_df)-len(tnegp_df)))
+
+print('Accuracy of positive predictions: ' +str(pos_acc)[:6]+'%')
+print('Accuracy of negative predictions: ' +str(neg_acc)[:6]+'%')
+print('Combined Accuracy of predictions: ' +str(com_acc)[:6]+'%')
+print('Positive Effectiveness: '  +str(pos_eff)[:6]+'%')
+print('Negative Effectiveness: '  +str(neg_eff)[:6]+'%')
+print('Long Only Effectiveness: ' +str(longonly_eff)[:6]+'%')
 
 'bye'
