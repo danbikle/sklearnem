366c905dd87382ec8bc90a62b53f45faade41f95
Dan Bikle <bikle@bikle.com>
2016-03-26 07:05:19 +0000

    hi

--- logistic_regression10.py
+++ logistic_regression10.py
@@ -7,11 +7,11 @@ from sklearn import linear_model
 clf = linear_model.LogisticRegression()
 clf.fit ([[0, 0], [1, 1], [2, 2]], [0, 0, 1])
 # Predict the training data:
-prediction0 = clf.predict_proba([[0.0,0.0]])
-prediction1 = clf.predict_proba([[1.0,1.0]])
-prediction2 = clf.predict_proba([[2.0,2.0]])
+prediction0 = clf.predict_proba([[0.0,0.0]]) # 0
+prediction1 = clf.predict_proba([[1.0,1.0]]) # 0
+prediction2 = clf.predict_proba([[2.0,2.0]]) # 1
 # Predict a test data point:
-prediction08 = clf.predict_proba([[0.8,0.8]])
+prediction08 = clf.predict_proba([[0.8,0.8]]) #?
 print(prediction0)
 print(prediction1)
 print(prediction2)
---
+++ logistic_regression11.py
@@ -0,0 +1,34 @@
+# logistic_regression11.py
+
+# This script should give demo of sklearn.linear_model.LogisticRegression
+
+# Ref:
+# http://scikit-learn.org/stable/modules/linear_model.html#ordinary-least-squares
+
+import numpy  as np
+import pandas as pd
+import prep4fit
+import rpt_acc_eff
+import pdb
+
+x_train_a = prep4fit.get_x_train_a()
+y_train_a = prep4fit.get_y_train_a()
+# I should learn from x_train_a,label_train_a:
+from sklearn import linear_model
+clf = linear_model.LogisticRegression()
+clf.fit(x_train_a, y_train_a)
+# Now that I have learned, I should predict:
+x_test_a = prep4fit.get_x_test_a()
+y_test_a = prep4fit.get_y_test_a()
+predictions_l = []
+for xoos_a in x_test_a:
+  xf_a        = xoos_a.astype(float)
+  xr_a        = xf_a.reshape(1, -1)
+
+
+# I should report accuracy and effectiveness.
+rpt_acc_eff.rpt_acc_eff(predictions_l)
+'bye'
+
+
+
