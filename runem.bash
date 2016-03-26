58f175009ac571b1e8e0e3f42b052d67d9669e02
Dan Bikle <bikle@bikle.com>
2016-03-26 08:19:44 +0000

    hi

---
+++ runem.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# runem.bash
+
+# This script should run the demos.
+set -x
+python naive_bayes11.py
+python linear_regression11.py
+python logistic_regression11.py
+python svm11.py
+python gbrt11.py
+exit
+
+
