3c359c35473d502fab5819f9973819c33fa695a8
Dan Bikle <bikle@bikle.com>
2016-03-26 06:26:11 +0000

    hi

---
+++ logistic_regression10.py
@@ -0,0 +1,11 @@
+# logistic_regression10.py
+
+# Ref:
+# http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+
+from sklearn import linear_model
+
+'bye'
+
+
+
