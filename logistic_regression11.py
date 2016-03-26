43adeec2b7fc932d76e67fc8f7188a6f5469cc06
Dan Bikle <bikle@bikle.com>
2016-03-26 07:09:11 +0000

    hi

--- logistic_regression11.py
+++ logistic_regression11.py
@@ -11,21 +11,22 @@ import prep4fit
 import rpt_acc_eff
 import pdb
 
-x_train_a = prep4fit.get_x_train_a()
-y_train_a = prep4fit.get_y_train_a()
+x_train_a     = prep4fit.get_x_train_a()
+label_train_a = prep4fit.get_label_train_a()
 # I should learn from x_train_a,label_train_a:
 from sklearn import linear_model
 clf = linear_model.LogisticRegression()
-clf.fit(x_train_a, y_train_a)
+clf.fit(x_train_a, label_train_a)
 # Now that I have learned, I should predict:
-x_test_a = prep4fit.get_x_test_a()
-y_test_a = prep4fit.get_y_test_a()
+x_test_a      = prep4fit.get_x_test_a()
+label_test_a  = prep4fit.get_label_test_a()
 predictions_l = []
 for xoos_a in x_test_a:
   xf_a        = xoos_a.astype(float)
   xr_a        = xf_a.reshape(1, -1)
-
-
+  aprediction = clf.predict_proba(xr_a)
+  pdb.set_trace()
+  aprediction
 # I should report accuracy and effectiveness.
 rpt_acc_eff.rpt_acc_eff(predictions_l)
 'bye'
