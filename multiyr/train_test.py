465c829913e3f26f9d381c27c3510cd03a74d7c6
Dan Bikle <bikle@bikle.com>
2016-03-27 09:57:08 +0000

    more

--- multiyr/train_test.py
+++ multiyr/train_test.py
@@ -16,3 +16,13 @@ if (len(sys.argv) < 3):
   print('python ~/sklearnem/multiyr/train_test.py 2010 2016')
   print('...')
   sys.exit()
+
+startyr = int(sys.argv[1])
+finalyr = int(sys.argv[2])
+
+# I should create a loop which does train and test for each yr.
+for yr in range(startyr,1+finalyr):
+  print(yr)
+
+'bye'
+
