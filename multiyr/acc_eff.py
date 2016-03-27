5707cdea7c5e3aab4a26093b6157c3b5e9a70170
Dan Bikle <bikle@bikle.com>
2016-03-27 18:43:36 +0000

    more

---
+++ multiyr/acc_eff.py
@@ -0,0 +1,23 @@
+# acc_eff.py
+
+# This script should report accuracy and effectiveness from CSV files in ~/ddata/
+
+import numpy  as np
+import pandas as pd
+import pdb
+
+# I should check cmd line arg
+import sys
+if (len(sys.argv) < 3):
+  print('Demo:')
+  print('cd ~/ddata')
+  print('python ~/sklearnem/multiyr/acc_eff 2010 2016')
+  print('...')
+  sys.exit()
+
+startyr = int(sys.argv[1])
+finalyr = int(sys.argv[2])
+
+# I should create a loop which does train and test for each yr.
+for yr in range(startyr,1+finalyr):
+  print(yr)
