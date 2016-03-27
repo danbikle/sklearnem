16fe61a93cc0a52d93f6f00311e1e6af044bd8cc
Dan Bikle <bikle@bikle.com>
2016-03-27 07:18:58 +0000

    more

--- multiyr/runem.bash
+++ multiyr/runem.bash
@@ -7,15 +7,21 @@
 
 # This script depends on wgetGSPCnight.bash
 # I should/can run       wgetGSPCnight.bash
-# before I run this script:
+# before I run runem.bash:
 # ~/sklearnem/multiyr/wgetGSPCnight.bash
 # I dont need to run it everytime though.
-
+# wgetGSPCnight.bash should give me ~/ddata/GSPC2.csv
 # I start by generating features from prices:
 cd     ~/ddata
 python ~/sklearnem/multiyr/genf.py GSPC2.csv
+# Now, I should have ftrGSPC2.csv 
 head   ftrGSPC2.csv
 tail   ftrGSPC2.csv
+
+# Next I generate training data CSV files:
+python ~/sklearnem/multiyr/gentrain.py ftrGSPC2.csv 30 2010 2016
+# The above call should give me a set of CSV files.
+# Each file should contain 30 years of data.
 
 exit
 
