6085bf2af688dd8b779754669047f4ceed151a2d
Dan Bikle <bikle@bikle.com>
2016-03-28 03:05:00 +0000

    more

--- multiyr/train_test.py
+++ multiyr/train_test.py
@@ -41,12 +41,13 @@ for yr in range(startyr,1+finalyr):
   # I should learn from x_train_a,label_train_a:
 
   from sklearn import linear_model
-  clf_lr = linear_model.LogisticRegression()
-
   from sklearn.naive_bayes import GaussianNB
-  clf_nb = GaussianNB()
 
+  clf_lr = linear_model.LogisticRegression()
+  clf_nb = GaussianNB()
+  clf_lr.fit(x_train_a, label_train_a)
   clf_nb.fit(x_train_a, label_train_a)
+
   # Now that I have learned, I should predict:
   testf    = 'test'+str(yr)+'.csv'
   test_df  = pd.read_csv(testf)
@@ -54,17 +55,21 @@ for yr in range(startyr,1+finalyr):
   x_test_a = test_a[:,pctlag1_i:end_i]
   y_test_a = test_a[:,pctlead_i]
   label_test_a  = y_test_a > train_median
+  predictions_lr_l = []
+  x_eff_lr_l       = [0.0]
+  recent_eff_lr_l  = [0.0]
+  acc_lr_l         = []
   predictions_nb_l = []
+  x_eff_nb_l       = [0.0]
+  recent_eff_nb_l  = [0.0]
+  acc_nb_l         = []
   xcount           = -1
-  x_eff_l          = [0.0]
-  recent_eff_nb_l     = [0.0]
-  acc_nb_l            = []
   for xoos_a in x_test_a:
     xcount        += 1 # should == 0 1st time through
     xf_a           = xoos_a.astype(float)
     xr_a           = xf_a.reshape(1, -1)
-    # aprediction = clf.predict_proba(xr_a)[0,1]
-    aprediction_nb    = clf_nb.predict(xr_a)
+    aprediction_lr = clf_lr.predict_proba(xr_a)[0,1]
+    aprediction_nb = clf_nb.predict(xr_a)
     # if (aprediction > 0.5):
     if (aprediction_nb[0] == True):
       predictions_nb_l.append(1)  # up   prediction
@@ -72,12 +77,12 @@ for yr in range(startyr,1+finalyr):
       predictions_nb_l.append(-1) # down prediction
     # I should save effectiveness of this prediction:
     pctlead = y_test_a[xcount]
-    x_eff_l.append(predictions_nb_l[xcount]*pctlead)
+    x_eff_nb_l.append(predictions_nb_l[xcount]*pctlead)
     # I should save recent effectiveness of this prediction:
     if (xcount < 5):
       recent_eff_nb_l.append(0.0)
     else:
-      recent_eff_nb_l.append(np.mean(x_eff_l[-5:]))
+      recent_eff_nb_l.append(np.mean(x_eff_nb_l[-5:]))
     # I should save accuracy of this prediction
     if ((y_test_a[xcount] > 0) and (aprediction_nb > 0.5)):
       acc_nb_l.append('tp')
@@ -88,10 +93,10 @@ for yr in range(startyr,1+finalyr):
     if ((y_test_a[xcount] < 0) and (aprediction_nb < 0.5)):
       acc_nb_l.append('tn')
   # I should save predictions_nb_l, eff, acc, so I can report later.
-  test_df['actual_dir']       = np.sign(test_df['pctlead'])
-  test_df['pdir_nb']          = predictions_nb_l
-  test_df['x_eff']            = x_eff_l[1:]
-  test_df['recent_eff_nb_nb'] = recent_eff_nb_l[1:]
+  test_df['actual_dir']    = np.sign(test_df['pctlead'])
+  test_df['pdir_nb']       = predictions_nb_l
+  test_df['x_eff_nb']      = x_eff_nb_l[1:]
+  test_df['recent_eff_nb'] = recent_eff_nb_l[1:]
   if (len(test_df) - len(acc_nb_l) == 1):
     # I should deal with most recent observation:
     acc_nb_l.append('unknown')
