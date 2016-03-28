d3aed8940c381220a86e2c7a3fae5bd29e73b05a
Dan Bikle <bikle@bikle.com>
2016-03-28 04:25:39 +0000

    mo

--- multiyr/runem.bash
+++ multiyr/runem.bash
@@ -36,7 +36,7 @@ python ~/sklearnem/multiyr/train_test.py $STARTYR $ENDYR
 python ~/sklearnem/multiyr/acc_eff.py $STARTYR $ENDYR
 
 # Then I should build blue-green visualizations.
-python ~/sklearnem/multiyr/bg.py $STARTYR $ENDYR
+python ~/sklearnem/multiyr/rgb.py $STARTYR $ENDYR
 
 exit
 
